f62a3dd450725d3ba8b3dc183e59607e7e4d6eee
dazcona <david.azcona@asu.edu>
2019-10-09 17:52:20 +0100

    misc data type

--- js/core.deobfuscated.js
+++ js/core.deobfuscated.js
@@ -54,7 +54,7 @@ number={2},
 pages={97},
 year={2007},
 publisher={Nature Publishing Group}
-}
+}  
 
 @phdthesis{corrigan2018investigation,
 title={An Investigation Into Machine Learning Solutions Involving Time Series Across Different Problem Domains},
@@ -139,16 +139,25 @@ function authors2html(authorData, vformat) {
         author = authorData[index];
         if (vformat == 'mla' || vformat == 'chicago') {
             if (index == 0) { authorsStr += author.last + ", " + author.first; } // First: Azcona, David
-            else { authorsStr += author.first + ", " + author.last; } // Rest: Piyush Arora
+            else { authorsStr += author.first + ((author.first && author.last) ? ", " : "") + author.last; } // Rest: Piyush Arora
         }
         else {
             initials = get_initials(author.first)
             if (vformat == 'vancouver') { separator = ""; } // Azcona, D
             else { separator = "."; } // Azcona, D.
-            authorsStr += author.last + ", " + initials.join(separator) + separator;
+            authorsStr += author.last + ((author.first) ? ", " + initials.join(separator) + separator : "");
         }
     }
     return htmlify(authorsStr);
+}
+
+function howpublished2readable(howpublished){
+    var howpublishedStr = '';
+    if (howpublished && howpublished.startsWith("\\url{") && howpublished.endsWith("}")) {
+        var uri = howpublished.split("\\url{")[1].split("}")[0];
+        howpublishedStr = '<a href="' + uri + '" target="_blank">' + uri + '</a>';
+    }
+    return htmlify(howpublishedStr);
 }
 
 // Function to format the citation based on the format selected
@@ -159,7 +168,6 @@ function format(data) {
 
     // Format authors
     var authors = authors2html(data.author, formatValue);
-    authors = ((authors) ? authors : "Authors are required!");
 
     // http://bib-it.sourceforge.net/help/fieldsAndEntryTypes.php#article
     // ARTICLE
@@ -167,6 +175,7 @@ function format(data) {
     // Required fields: author, title, journal, year.
     // Optional fields: volume, number, pages, month, note.
     if (data.entryType == "article") {
+        authors = ((authors) ? authors : "Authors are required!");
         var title = ((data.title) ? data.title : "<strong style='color:red;'>Title is required!</strong>");
         var journal = ((data.journal) ? data.journal : "<strong style='color:red;'>Journal is required!</strong>")
         var year = ((data.year) ? data.year : "<strong style='color:red;'>Year is required!</strong>")
@@ -229,6 +238,7 @@ function format(data) {
     // Required fields: author, title, booktitle, year.
     // Optional fields: editor, volume or number, series, pages, address, month, organization, publisher, note.
     else if (data.entryType == "inproceedings") {
+        authors = ((authors) ? authors : "Authors are required!");
         var title = ((data.title) ? data.title : "<strong style='color:red;'>Title is required!</strong>");
         var booktitle = ((data.booktitle) ? data.booktitle : "<strong style='color:red;'>Book title is required!</strong>");
         var year = ((data.year) ? data.year : "<strong style='color:red;'>Year is required!</strong>");
@@ -282,6 +292,7 @@ function format(data) {
     // Required fields: author or editor, title, publisher, year.
     // Optional fields: volume or number, series, address, edition, month, note.
     else if (data.entryType == "book") {
+        authors = ((authors) ? authors : "Authors are required!");
         var title = ((data.title) ? data.title : "<strong style='color:red;'>Title is required!</strong>");
         var publisher = ((data.publisher) ? data.publisher : "<strong style='color:red;'>Publisher is required!</strong>");
         var year = ((data.year) ? data.year : "<strong style='color:red;'>Year is required!</strong>");
@@ -329,6 +340,7 @@ function format(data) {
     // Required fields: author, title, school, year.
     // Optional fields: type, address, month, note.
     else if (data.entryType == 'phdthesis') {
+        authors = ((authors) ? authors : "Authors are required!");
         var title = ((data.title) ? data.title : "<strong style='color:red;'>Title is required!</strong>");
         var school = ((data.school) ? data.school : "<strong style='color:red;'>School is required!</strong>");
         var year = ((data.year) ? data.year : "<strong style='color:red;'>Year is required!</strong>");
@@ -362,6 +374,31 @@ function format(data) {
                 " (Doctoral dissertation, " + school + ").";
         }
     }
+    // MISC
+    // Use this type when nothing else fits. A warning will be issued if all optional fields are empty 
+    // (i.e., the entire entry is empty or has only ignored fields).
+    // Required fields: none.
+    // Optional fields: author, title, howpublished, month, year, note.
+    else if (data.entryType == 'misc') {
+        if (formatValue == 'mla' || formatValue == 'chicago') {
+            return ((authors) ? authors + ". ": "") + 
+                ((data.title) ? "\"" + data.title + ".\" ": "") +  
+                ((data.howpublished) ? howpublished2readable(data.howpublished) + ". ": "") +
+                ((data.year) ? " (" + data.year + "). ": "");
+        }
+        else if (formatValue == 'apa'|| formatValue == 'harvard') {
+            return ((authors) ? authors + ". ": "") + 
+                ((data.year) ? " (" + data.year + "). ": "") +
+                ((data.title) ? data.title + ". ": "") +  
+                ((data.howpublished) ? howpublished2readable(data.howpublished) + ". ": "");
+        }
+        else if (formatValue == 'vancouver') {
+            return ((authors) ? authors + ". ": "") + 
+                ((data.title) ? data.title + ". ": "") +  
+                ((data.howpublished) ? howpublished2readable(data.howpublished) + ". ": "");
+        }
+    }
+    // Otherwise
     else {
         return 'Format ' + data.entryType + ' not supported yet!'
     }
